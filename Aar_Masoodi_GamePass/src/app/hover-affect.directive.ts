e5eb040e235e0693c0c58e0220813cae39bf6f60
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-02 18:14:00 -0400

    tags now become bold when hovered over using the same directive.

--- Aar_Masoodi_GamePass/src/app/hover-affect.directive.ts
+++ Aar_Masoodi_GamePass/src/app/hover-affect.directive.ts
@@ -1,29 +1,47 @@
 import { Directive } from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
 
+  @Input() styleType: string|undefined;
+
   constructor(private elm: ElementRef) {
    }
 
-
-   @HostListener('mouseenter') onMouseEnter() {
-    this.underLineMe();
-
-   }
-   @HostListener('mouseleave') onMouseLeave() {
-      this.removeDecoration();
-
-   }
    private underLineMe() {
     this.elm.nativeElement.style.textDecoration = "underline";
+   }
+   private boldMe() {
+    this.elm.nativeElement.style.fontWeight = "bold";
    }
    private removeDecoration() {
     this.elm.nativeElement.style.textDecoration = "none";
    }
+   private removeFontWeight() {
+    this.elm.nativeElement.style.fontWeight = "normal";
+   }
+
+   @HostListener('mouseenter') onMouseEnter() {
+    if (this.styleType == "underline") {
+      this.underLineMe();
+    }
+    if (this.styleType == "bold") {
+      this.boldMe();
+    }
+
+    
+
+   }
+   @HostListener('mouseleave') onMouseLeave() {
+      this.removeDecoration();
+      this.removeFontWeight();
+
+   }
+   
 
 }
