afadce4e2b2bd48d8f82c39dac96f4d4f1539e19
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-19 19:35:12 -0400

    working on getting the add new content working

---
+++ Aar_Masoodi_GamePass/src/app/modify-content-component/modify-content-component.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { Output, EventEmitter } from '@angular/core';
+import { ContentService } from '../GamePassService/content.service';
+
+@Component({
+  selector: 'app-modify-content-component',
+  templateUrl: './modify-content-component.component.html',
+  styleUrls: ['./modify-content-component.component.scss']
+})
+export class ModifyContentComponentComponent {
+
+  @Output() addGameEvent = new EventEmitter<Content>();
+
+  constructor(private contentService: ContentService) {};
+
+  addContentToList(newContentItem: Content): void {
+    this.contentService.addContent(newContentItem).subscribe(newContentFromServer => this.push(newContentFromServer))
+  }
+
+
+
+}
