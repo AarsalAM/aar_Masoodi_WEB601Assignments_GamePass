6189b01404c91561da5db4fbfab66f7fe984e175
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-19 20:35:58 -0400

    Assignment 7 done

--- Aar_Masoodi_GamePass/src/app/app.component.ts
+++ Aar_Masoodi_GamePass/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ContentService } from './GamePassService/content.service';
-import { Observable } from 'rxjs';
-import { Content } from './helper-files/content-interface';
-import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -11,22 +7,4 @@ import { MessageService } from './message.service';
 })
 export class AppComponent {
   title = 'Aar_Masoodi_GamePass';
-
-  game$: Observable<Content>;
-
-  constructor(private contentService: ContentService,
-              private messageService: MessageService
-              ) {}
-
-
-  // getGameObs(id: number) {
-  //   let gameFound;
-  //   this.contentService.getGameObs(id).subscribe((game) => {
-  //     gameFound = game;
-  //     // this.messageService.add("New content loaded in");
-  //   });
-  //   return gameFound;
-  // }
-
-  
 }
