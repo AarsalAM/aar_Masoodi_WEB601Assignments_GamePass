9e468e445b8422ddaf0fd8bc941f2cf6760c2c02
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-25 19:22:14 -0400

    utilizing the service SwUpdate in the newly created service

--- Aar_Masoodi_GamePass/src/app/log-update.service.ts
+++ Aar_Masoodi_GamePass/src/app/log-update.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogUpdateService {
 
-  constructor() { }
+  constructor(private updates: SwUpdate) { }
+
+  public init(){
+    this.updates.versionUpdates.subscribe(event => {
+      switch (event.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading the new app version: ${event.version.hash}`);
+          break;
+          case 'VERSION_READY':
+            console.log(`current app version is: ${event.currentVersion.hash}`);
+            console.log(`the new app version that is ready for use is: ${event.latestVersion.hash}`);
+            break;
+      }
+    });
+  }
 }
