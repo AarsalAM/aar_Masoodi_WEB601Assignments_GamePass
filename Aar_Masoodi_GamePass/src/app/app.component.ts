d6f6fe21fbc7f3f392529eb0ade2d2e8c0aaa1a2
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-25 20:01:33 -0400

    Added functionality for the application to auto check for updates every half hour (BONUS)

--- Aar_Masoodi_GamePass/src/app/app.component.ts
+++ Aar_Masoodi_GamePass/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { ContentService } from './GamePassService/content.service';
 import { LogUpdateService } from './log-update.service';
+import { SwUpdate } from '@angular/service-worker';
+import { concat, first, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,19 @@ import { LogUpdateService } from './log-update.service';
 export class AppComponent {
   title = 'Aar_Masoodi_GamePass';
 
-  constructor(private contentService: ContentService, private logService: LogUpdateService) {}
+  constructor(private contentService: ContentService, 
+              private logService: LogUpdateService,
+              private appRef: ApplicationRef,
+              private updates: SwUpdate) {}
 
   ngOnInit(): void {
     this.logService.init();
+
+    const appIsStable$ = this.appRef.isStable.pipe(first(isStable => isStable === true));
+    const everyHalfHour$ = interval(0.5 * 60 * 60 * 1000);
+    const everyHalfHourAppIsStable$ = concat(appIsStable$, everyHalfHour$);
+
+    everyHalfHourAppIsStable$.subscribe( ()=> this.updates.checkForUpdate());
+    
   }
 }
