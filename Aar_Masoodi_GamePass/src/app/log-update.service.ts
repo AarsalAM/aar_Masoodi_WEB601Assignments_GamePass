2a908110485138e9809751ce210238fb90356bbe
AarsalM <91852812+AarsalM@users.noreply.github.com>
2023-04-25 21:14:24 -0400

    Assignment 10 complete. Including Bonus

--- Aar_Masoodi_GamePass/src/app/log-update.service.ts
+++ Aar_Masoodi_GamePass/src/app/log-update.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,12 @@ export class LogUpdateService {
           console.log(`Downloading the new app version: ${event.version.hash}`);
           break;
           case 'VERSION_READY':
-            this.openSnackBar;
+            this.updates.versionUpdates.subscribe(() => {
+              const snack = this.snackBar.open('New version available', 'Reload');
+              snack.onAction().subscribe(() => {
+                window.location.reload();
+                      });
+                    });
             console.log(`current app version is: ${event.currentVersion.hash}`);
             console.log(`the new app version that is ready for use is: ${event.latestVersion.hash}`);
             break;
@@ -29,6 +34,6 @@ export class LogUpdateService {
     const snackBarRef = this.snackBar.open('An Update is Available', 'Update');
     snackBarRef.onAction().subscribe(()=> {
       this.updates.activateUpdate().then(()=> document.location.reload());
-    })
+    });
   }
 }
